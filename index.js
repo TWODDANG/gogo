ce6599b2d2194c0715a970f4a0067bc5b910bdb0
2dd <79iug12@gmail.com>
2019-02-23 17:39:09 +0900

    다시 시작

---
+++ index.js
@@ -0,0 +1,31 @@
+// Dependencies
+const express = require('express');
+const bodyparser = require('body-parser');
+
+// Configuration
+const app = express();
+app.use(bodyparser.json());
+app.get("/", function(req,res){
+  console.log("hihi");
+})
+
+// Webhook route
+app.post('/webhook', (req, res) => {
+	const data = req.body;
+  console.log(data);
+
+	// Code the task you want to achieve with @data
+	// Read the v2 api documentation of dialogflow : https://dialogflow.com/docs/fulfillment
+	// Using the v2 will become mandatory, Google wrote a guide to migrate from v1 to v2 as v2 is officially released
+
+	const response = {
+		fulfillmentText: "Your webhook works fine !",
+	}
+	res.json(response);
+  console.log("연결되었습니다!");
+});
+console.log("실행포트번호눈" + process.env.PORT);
+app.listen(process.env.PORT || 3000);
+
+
+// Logic for running your server with HTTPS here
