ffdc5dbbf41ab374ae42b8ea12ae58249b894184
2dd <79iug12@gmail.com>
2019-02-24 20:18:02 +0900

    second

--- index.js
+++ index.js
@@ -2,10 +2,10 @@
 const express = require('express');
 const bodyparser = require('body-parser');
 
-const { WebhookClient } = require('dialogflow-fulfillment');
+
 
  //Create an instance
-const agent = new WebhookClient({request: request, response: response});
+
 
 var http = require("http");
 setInterval(function() {
@@ -24,21 +24,47 @@ app.post('/', (req, res) => {
   var intent = data.queryResult.intent.displayName;
   var queryText = data.queryResult.queryText;
   var parameters = data.queryResult.parameters;
-  agent.add(`how are you doing`);
-  agent.add("hi");
 
 
 	// Code the task you want to achieve with @data
 	// Read the v2 api documentation of dialogflow : https://dialogflow.com/docs/fulfillment
 	// Using the v2 will become mandatory, Google wrote a guide to migrate from v1 to v2 as v2 is officially released
-/*
+
 	const response = {
 	fulfillmentText: "너의 intent는 " + intent
 
 	}
-	res.json(response);
-  res.json(response);
-  */
+  let quickReply = [
+      {
+        "payload": {},
+        "platform": "FACEBOOK"
+      },
+      {
+        "quickReplies": {
+          "title": "골라봐",
+          "quickReplies": [
+            "가위",
+            "바위",
+            "보"
+          ]
+        },
+        "platform": "FACEBOOK"
+      },
+      {
+        "text": {
+          "text": [
+            ""
+          ]
+        }
+      },
+      {
+        "payload": {}
+      }
+    ]
+
+	res.json(quickReply);
+
+
   console.log("연결되었습니다!");
 });
 console.log("실행포트번호눈" + process.env.PORT);
