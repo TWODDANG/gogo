ca83c9a3b207c277a298d5304780197024a7ee81
2dd <79iug12@gmail.com>
2019-02-24 22:59:13 +0900

    second

--- index.js
+++ index.js
@@ -91,7 +91,7 @@ app.use(express.json());
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
-app.listen(process.env.PORT, () => console.log('Example app listening on port 3000!'))
+app.listen(process.env.PORT, () => console.log('Example app listening on port 3000!' + process.env.PORT));
 
 // POST method route
 app.post('/', function (req, res) {
