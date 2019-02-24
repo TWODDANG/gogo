45f61afb381c7b7fac537bee9122fd63fd12995d
2dd <79iug12@gmail.com>
2019-02-24 11:12:19 +0900

    second

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const bodyparser = require('body-parser');
 
-const { WebhookClient } = require('dialogflow-webhook');
+const { WebhookClient } = require('dialogflow-fulfillment');
 
  //Create an instance
 const agent = new WebhookClient({request: request, response: response});
@@ -31,13 +31,14 @@ app.post('/', (req, res) => {
 	// Code the task you want to achieve with @data
 	// Read the v2 api documentation of dialogflow : https://dialogflow.com/docs/fulfillment
 	// Using the v2 will become mandatory, Google wrote a guide to migrate from v1 to v2 as v2 is officially released
-
+/*
 	const response = {
 	fulfillmentText: "너의 intent는 " + intent
 
 	}
 	res.json(response);
   res.json(response);
+  */
   console.log("연결되었습니다!");
 });
 console.log("실행포트번호눈" + process.env.PORT);
