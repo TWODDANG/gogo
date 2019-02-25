a31cc7e7f3d4a548c78f3151dc0a635914dc8b5a
2dd <79iug12@gmail.com>
2019-02-25 20:13:33 +0900

    second

--- index.js
+++ index.js
@@ -36,31 +36,61 @@ app.post('/', (req, res) => {
 	// Using the v2 will become mandatory, Google wrote a guide to migrate from v1 to v2 as v2 is officially released
 
 	const response = {
-	//fulfillmentText: "너의 intent는 " + intent,
-
-
-  "fulfillmentMessages": [
-    {
-      "quickReplies": {
-        "title": "ㅇ",
-        "quickReplies": [
-          "ㅇ"
-        ]
-      },
-      "platform": "FACEBOOK"
-    },
-    {
-      "text": {
-        "text": [
-          ""
-        ]
+"fulfillmentText": "This is a text response",
+"fulfillmentMessages": [
+  {
+    "card": {
+      "title": "card title",
+      "subtitle": "card text",
+      "imageUri": "https://assistant.google.com/static/images/molecule/Molecule-Formation-stop.png",
+      "buttons": [
+        {
+          "text": "button text",
+          "postback": "https://assistant.google.com/"
+        }
+      ]
+    }
+  }
+],
+"source": "example.com",
+"payload": {
+  "google": {
+    "expectUserResponse": true,
+    "richResponse": {
+      "items": [
+        {
+          "simpleResponse": {
+            "textToSpeech": "this is a simple response"
+          }
+        }
+      ]
+    }
+  },
+  "facebook": {
+    "text": "Hello, Facebook!"
+  },
+  "slack": {
+    "text": "This is a text response for Slack."
+  }
+},
+"outputContexts": [
+  {
+    "name": "projects/${PROJECT_ID}/agent/sessions/${SESSION_ID}/contexts/context name",
+    "lifespanCount": 5,
+    "parameters": {
+      "param": "param value"
+    }
+  }
+],
+"followupEventInput": {
+  "name": "event name",
+  "languageCode": "en-US",
+  "parameters": {
+    "param": "param value"
       }
     }
 
-]
-};
-
-
+}
   //  responseJson.fulfillmentMessages = quickReply;
     //res.json(responseJson);
 	res.json(response);
