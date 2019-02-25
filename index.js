f8fc625898381a0764e5bf3175808383c57054ad
2dd <79iug12@gmail.com>
2019-02-25 22:04:16 +0900

    second

--- index.js
+++ index.js
@@ -36,6 +36,7 @@ app.post('/', (req, res) => {
 	// Using the v2 will become mandatory, Google wrote a guide to migrate from v1 to v2 as v2 is officially released
 
 	const response = {
+    /*
 "fulfillmentText": "This is a text response",
 
 /*
